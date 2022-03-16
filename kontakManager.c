dfeda51012d037a523f8189da8c162018b0fdb66
itshiroto <rivowowor7@gmail.com>
2022-03-16 13:27:22 +0800

    Scripted auto-commit on change (2022-03-16 13:27:22) by gitwatch.sh

--- kontakManager.c
+++ kontakManager.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct Contact {
+typedef struct Contact
+{
     char nama[50];
     char domisili[50];
     char hubungan[50];
@@ -12,17 +13,60 @@ typedef struct Contact {
     char jenisKelamin;
 } Contact;
 
-int mainMenu() {
+typedef struct Node {
+    Contact data;
+    struct Node *next, *prev;
+} Node;
+
+int mainMenu()
+{
     printf(
         "========================================================\n"
-        "|                     Kontak Manager                   |\n"    
+        "|                     Kontak Manager                   |\n"
         "|                         v0.1a                        |\n"
-        "========================================================\n"
-        );
+        "========================================================\n");
     printf(
         "1. Data kontak\n"
         "2. Tambah kontak\n"
         "3. Hapus kontak\n"
-        "4. About me\n"
-    );
+        "4. About me\n");
+    
+    int pilihan;
+    printf("\nPilihan: ");
+    scanf("%d", &pilihan);
+    return pilihan;
+}
+
+void appendNode(Contact data, Node **head, Node **tail) {
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = newNode->prev = NULL;
+    if(*head == NULL) {
+        *head = *tail = newNode;
+    } else {
+        newNode->prev = *tail;
+        newNode->next = *head;
+        (*tail)->next = newNode;
+        (*head)->prev = newNode;
+        *tail = newNode;
+    }
+}
+
+void printData(Node **head) {
+    Node *current = *head;
+    if (*head == NULL) {
+        printf("\nData kontak kosong\n");
+        return;
+    }
+    do {
+        printf("Nama: %s\n", current->data.nama);
+        printf("Domisili: %s\n", current->data.domisili);
+        printf("Hubungan: %s\n", current->data.hubungan);
+        printf("Nomor: %s\n", current->data.nomor);
+        printf("Email: %s\n", current->data.email);
+        printf("Workplace: %s\n", current->data.workplace);
+        printf("Jenis Kelamin: %c\n", current->data.jenisKelamin);
+        printf("========================================================\n");
+        current = current->next;
+    } while(current != *head);
 }
