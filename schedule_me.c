5e00de2f4189fbf95920cf51158717286e3b710f
itshiroto <rivowowor7@gmail.com>
2022-03-22 15:48:25 +0800

    Scripted auto-commit on change (2022-03-22 15:48:25) by gitwatch.sh

--- schedule_me.c
+++ schedule_me.c
@@ -66,7 +66,7 @@ int main() {
                 queuePos++;
             }
         }
-        if (queue[curr].arrivalTime == i) {
+        if (queue[curr].runTime == i) {
             curr += 1;
         }
     }
