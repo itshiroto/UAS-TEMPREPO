a65da4d8535ff64b704b9217f950e99927aa1cbb
itshiroto <rivowowor7@gmail.com>
2022-03-22 13:42:08 +0800

    Scripted auto-commit on change (2022-03-22 13:42:08) by gitwatch.sh

--- drink_coffee_contest.c
+++ drink_coffee_contest.c
@@ -22,7 +22,7 @@ void printCoffee() {
 }
 
 void drink_coffee(int *player) {
-  if (coffee_available == 0) return;
+  if (coffee_available < 0) return;
   sem_wait(&mutex);
   coffee_drunk[*player] += 1;
   coffee_available -= 1;
@@ -30,7 +30,7 @@ void drink_coffee(int *player) {
 }
 
 void *player(void *drunkArr) {
-  while (coffee_available != 0) {
+  while (coffee_available <= 0) {
     int *player = drunkArr;
     drink_coffee(player);
     printCoffee();
