6c4f375451e1f1ce7a4e51778d9bcd11ec19c82d
itshiroto <rivowowor7@gmail.com>
2022-03-22 15:35:12 +0800

    Scripted auto-commit on change (2022-03-22 15:35:12) by gitwatch.sh

--- schedule_me.c
+++ schedule_me.c
@@ -22,7 +22,7 @@ int main() {
     }
 
     for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) {
+        for (int j = i; j < 6; j++) {
             if (jobs[i].runTime > jobs[j].runTime) {
                 job temp = jobs[i];
                 jobs[i] = jobs[j];
