458506f2b6493b2f755b716051b8603201401fe8
itshiroto <rivowowor7@gmail.com>
2022-03-22 13:14:46 +0800

    Scripted auto-commit on change (2022-03-22 13:14:46) by gitwatch.sh

---
+++ drink_coffee_contest.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+#define MAX_COFFEE 100
+#define MIN_COFFEE 3
+#define PLAYERS_THREAD 5
+
+// Create a program that will create 5 thread, and all of them will compete to drink most coffee
+// The program has constraints of coffee available, 3 < n < 100
+// The program must fair and no thread is taking all of the coffee
+// The program must be able to print the coffee taken by each thread
+// The program must announce the winner from first to third place
+
+int coffee_available, coffee_taken[PLAYERS_THREAD];
+
+int main() {
+    pthread_t players[PLAYERS_THREAD];
+    sem_t mutex;
+}
