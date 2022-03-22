a0a360eb7185fa30cb387cc6c5e6749fa670ab8a
itshiroto <rivowowor7@gmail.com>
2022-03-22 14:43:39 +0800

    Scripted auto-commit on change (2022-03-22 14:43:39) by gitwatch.sh

---
+++ schedule.me.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    // create a program that would simulate CPU scheduling
+    // using the Shortest Job First (SJF) algorithm
+    // and the process must be printed in "time - executed job" format
+    // and the program must calculate the average turnaround time
+    // and the average waiting time
+    // 
+    
+}
