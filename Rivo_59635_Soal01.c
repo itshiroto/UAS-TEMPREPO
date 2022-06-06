8c9415214d3eff3e5c03b757dfeff440c3f38fd2
itshiroto <rivowowor7@gmail.com>
2022-06-06 14:40:02 +0800

    Scripted auto-commit on change (2022-06-06 14:40:02) by gitwatch.sh

--- Rivo_59635_Soal01.c
+++ Rivo_59635_Soal01.c
@@ -79,7 +79,6 @@ void readFile(Kota **dbKota, long ongkirArr[][100], int *size) {
     printf("File %s tidak ditemukan!\n", ONGKIR_NAME);
     exit(1);
   }
-  // ongkirArr = createMatrix(i, i);
   *size = i;
   for (i = 0; i < *size; i++) {
     for (j = 0; j < *size; j++) {
