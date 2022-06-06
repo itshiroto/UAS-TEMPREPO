4121b5d3f99c99bba964b1762b6f009d63c3f8ed
itshiroto <rivowowor7@gmail.com>
2022-06-06 15:12:20 +0800

    Scripted auto-commit on change (2022-06-06 15:12:20) by gitwatch.sh

--- Rivo_59635_Soal02.c
+++ Rivo_59635_Soal02.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <conio.h>
+#include <windows.h>
+#define mkdir(dir, mode) !CreateDirectory(dir, NULL)
+#define CLEAR "cls"
+#define PLATFORM_NAME "win"
+#elif defined(__linux__)
+#define CLEAR "clear"
+#define PLATFORM_NAME "linux"
+#endif
+
+void pause() {
+  if (strcmp(PLATFORM_NAME, "win") == 0) {
+    system("pause");
+  } else {
+    printf("Press any key to continue...");
+    getchar();
+  }
+}
