ece4e528e10caafbe56149427f0b2590eaf1110a
itshiroto <rivowowor7@gmail.com>
2022-06-06 15:27:03 +0800

    Scripted auto-commit on change (2022-06-06 15:27:03) by gitwatch.sh

--- Rivo_59635_Soal02.c
+++ Rivo_59635_Soal02.c
@@ -21,3 +21,41 @@ void pause() {
     getchar();
   }
 }
+
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
+
+void insertToList(Node **head, int data) {
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  if (head == NULL) {
+    *head = newNode;
+  } else {
+    Node *curr = *head;
+    while (curr->next != NULL) {
+      curr = curr->next;
+    }
+    curr->next = *newNode;
+  }
+}
+
+int main() {
+  Node *bucket[10];
+  int i;
+  for (i = 0; i < 10; i++) {
+    bucket[i] = NULL;
+  }
+  int jodoh = 0;
+  printf("Selamat datang di aplikasi penjodoh bilangan");
+  printf(
+      "Silahkan masukkan satuan dari jumlah bilangan yang akan dijodohkan: \n");
+  scanf("%d%*c", &jodoh);
+  int input = 0;
+  i = 0;
+  while (input >= 0) {
+    printf("Masukkan bilangan ke-%d: ", i + 1);
+    scanf("%d%*c", &input);
+  }
+}
