3d3e98d14d1c88989ce07970399e9455c338bebd
itshiroto <rivowowor7@gmail.com>
2022-06-09 13:13:49 +0800

    Scripted auto-commit on change (2022-06-09 13:13:49) by gitwatch.sh

--- disk_scheduling.c
+++ disk_scheduling.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_DISK_SIZE 300
+#define MAX_REQ 10
+
+void disk_CSCAN(int arr[], int head) { int seek_count = 0; }
 
 int main() {
   printf("Hello Lab!\n");
