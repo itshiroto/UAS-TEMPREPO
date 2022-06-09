75c370aab583cc3fd2ac435345147e52ff811150
itshiroto <rivowowor7@gmail.com>
2022-06-09 14:42:45 +0800

    Scripted auto-commit on change (2022-06-09 14:42:45) by gitwatch.sh

--- page_replacement_lru.c
+++ page_replacement_lru.c
@@ -1,3 +1,57 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct Node {
+  int data;
+  struct Node *next;
+} Node;
+
+int pageFault = 0;
+
+void insertNode(Node **head, int data) {
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  if (*head == NULL) {
+    *head = newNode;
+  } else {
+    Node *temp = *head;
+    while (temp->next != NULL) {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+  }
+}
+
+void removeNode(Node **head, int data) {
+  Node *temp = *head;
+  while (temp != NULL) {
+    if (temp->data == data) {
+      if (temp == *head) {
+        *head = temp->next;
+      } else {
+        Node *prev = *head;
+        while (prev->next != temp) {
+          prev = prev->next;
+        }
+        prev->next = temp->next;
+      }
+      free(temp);
+      break;
+    }
+    temp = temp->next;
+  }
+}
+
+bool checkIfExists(Node *head, int data) {
+  Node *temp = head;
+  while (temp != NULL) {
+    if (temp->data == data) {
+      return true;
+    }
+    temp = temp->next;
+  }
+  return false;
+}
