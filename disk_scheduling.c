f6fa13936c51c8f257381c27403410e2bafe64c2
itshiroto <rivowowor7@gmail.com>
2022-06-09 13:20:30 +0800

    Scripted auto-commit on change (2022-06-09 13:20:30) by gitwatch.sh

--- disk_scheduling.c
+++ disk_scheduling.c
@@ -4,7 +4,36 @@
 #define MAX_DISK_SIZE 300
 #define MAX_REQ 10
 
-void disk_CSCAN(int arr[], int head) { int seek_count = 0; }
+typedef struct dev_req {
+  int *req;
+  int head;
+  int size;
+} dev_req;
+
+void CSCAN_init(dev_req in, int **left, int **right, int *leftSize,
+                int *rightSize) {
+  *left = malloc(sizeof(**left) * in.size);
+  *right = malloc(sizeof(**right) * in.size);
+  int i;
+  for (i = 0; i < in.size; i++) {
+    if (in.req[i] < in.head) {
+      (*left)[*leftSize] = in.req[i];
+      (*leftSize)++;
+    } else {
+      (*right)[*rightSize] = in.req[i];
+      (*rightSize)++;
+    }
+  }
+  *left = (int *)realloc((*left), sizeof(**left) * (*leftSize));
+  *right = (int *)realloc((*right), sizeof(**right) * (*rightSize));
+}
+
+void disk_CSCAN(int arr[], int head, int arrSize) {
+  int seekCnt = 0;
+  int distance, currTrack;
+  int left[MAX_DISK_SIZE];
+  int right[MAX_DISK_SIZE];
+}
 
 int main() {
   printf("Hello Lab!\n");
